07b5067e43ec8e1a1d3936d110a71abb53a5f22f
Mossaab Bouhia <blackstar@kali.Home>
2024-08-12 20:41:08 +0100

    libft

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,13 +1,6 @@
 #include <string.h>
-
+#include "libft.h"
 void ft_bzero(void *s, size_t n)
 {
-  unsigned char *ptr;
-  ptr = (unsigned char *)s;
-  while(n > 0)
-  {
-    *ptr = 0;
-    ptr++;
-    n--;
-  }
+  ft_memset(s, 0, n);
 }
